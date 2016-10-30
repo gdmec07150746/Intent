ad8870e82dad9c9781460150e5c1cb590a226660
Pangzi <846162692@qq.com>
2016-10-30 13:43:22 +0800

    Initial commit

---
+++ app/src/main/java/s07150746/intent/IntentDemo3.java
@@ -0,0 +1,14 @@
+package s07150746.intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by abc on 2016/10/29.
+ */
+public class IntentDemo3  extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
